21c08a50ea9db64cffe6ff2a0f27dc9b179ed3b0
AlaaAEid <alaa_eid1@outlook.com>
2021-01-29 05:56:17 +0200

    Add project files.

---
+++ Libraries/Nop.Services/ExportImport/TypeOfExportedAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace Nop.Services.ExportImport
+{
+    /// <summary>
+    /// Represents the type of the exported attribute
+    /// </summary>
+    public enum ExportedAttributeType
+    {
+        /// <summary>
+        /// Not specified
+        /// </summary>
+        NotSpecified = 1,
+
+        /// <summary>
+        /// Product attribute
+        /// </summary>
+        ProductAttribute = 10,
+
+        /// <summary>
+        /// Product attribute
+        /// </summary>
+        ProductAttributeCombination = 15,
+
+        /// <summary>
+        /// Specification attribute
+        /// </summary>
+        SpecificationAttribute = 20
+    }
+}
